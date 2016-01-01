dc81b19bd083ac5fe9f6789d292d98ef97b3dc38
Andreas Braun <git@alcaeus.org>
2016-01-01 13:09:35 +0100

    Implement MongoDB::command()

--- tests/Alcaeus/MongoDbAdapter/MongoDBTest.php
+++ tests/Alcaeus/MongoDbAdapter/MongoDBTest.php
@@ -23,6 +23,24 @@ class MongoDBTest extends TestCase
         $this->assertSame('mongo-php-adapter.test', (string) $collection);
     }
 
+    public function testCommand()
+    {
+        $db = $this->getDatabase();
+        $this->assertEquals(['ok' => 1], $db->command(['ping' => 1], [], $hash));
+    }
+
+    public function testCommandError()
+    {
+        $db = $this->getDatabase();
+        $expected = [
+            'ok' => 0,
+            'errmsg' => 'listDatabases may only be run against the admin database.',
+            'code' => 13,
+        ];
+
+        $this->assertEquals($expected, $db->command(['listDatabases' => 1], [], $hash));
+    }
+
     /**
      * @return \MongoDB
      */
