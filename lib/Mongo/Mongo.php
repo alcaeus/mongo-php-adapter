a62828a1ae39de3ade37d9c52c366719ea365d64
Maks3w <github.maks3w@virtualplanets.net>
2016-02-06 08:02:44 +0100

    Allow create Mongo instances
    Make it compatible with Doctrine ODM

--- lib/Mongo/Mongo.php
+++ lib/Mongo/Mongo.php
@@ -24,14 +24,6 @@
 class Mongo extends MongoClient
 {
     /**
-     * Dummy constructor to throw an exception
-     */
-    public function __construct()
-    {
-        $this->notImplemented();
-    }
-
-    /**
      * Get pool size for connection pools
      *
      * @link http://php.net/manual/en/mongo.getpoolsize.php
